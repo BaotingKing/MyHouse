2816a06ce085be1ebf02976939a3a75331893b77
BaotingKing <baoting.zhang@westwell-lab.com>
2018-07-25 18:17:38 +0800

    some cs tools

--- nnProject/ComputerVision/mianVidio.py
+++ nnProject/ComputerVision/mianVidio.py
@@ -10,30 +10,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-# cap = cv2.VideoCapture('vtest.avi')
-# while True:
-#     ret, colr_img = cap.read()
-#     cv2.imshow('show_img', colr_img)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
-
-
-'''写视频'''
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640,480))
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret == True:
-        out.write(frame)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
+cap = cv2.VideoCapture('vtest.avi')
+while True:
+    ret, colr_img = cap.read()
+    cv2.imshow('show_img', colr_img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 cap.release()
-out.release()
 cv2.destroyAllWindows()
+
+
+# '''写视频'''
+# cap = cv2.VideoCapture(0)
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('output.avi', fourcc, 30.0, (640,480))
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     if ret == True:
+#         out.write(frame)
+#         cv2.imshow('frame', frame)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     else:
+#         break
+#
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
