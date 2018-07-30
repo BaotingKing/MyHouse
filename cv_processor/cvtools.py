29ca5561c414f2fc7e9b21eb072d9897e744889f
BaotingKing <baoting.zhang@westwell-lab.com>
2018-07-30 15:24:55 +0800

    add some little toys

--- cv_processor/cvtools.py
+++ cv_processor/cvtools.py
@@ -6,7 +6,7 @@ Created on 2018-07-25 星期三 14：04
 import sys, cv2, os, time, sqlite3, collections, hashlib
 # import cpuid, pika
 # import ConfigParser
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from random import shuffle
 # from Crypto.Cipher import AES
 # from Crypto import Random
@@ -39,15 +39,11 @@ def UpandDownPoint(Boxes):
 def DownandUpPoint():
     pass
 
-
 def ExtendBox(Boxes, dim, pixelthd=36):
     """将box的某一边放大到pixelthd大小"""
     for i in range(Boxes.shape[0]):
         single_box = Boxes[i, :]
-        if single_box[dim] > single_box[dim + 2]:
-            edgelength = single_box[dim] - single_box[dim + 2]
-        else:
-            edgelength = single_box[dim + 2] - single_box[dim]
+        edgelength = abs(single_box[dim] - single_box[dim + 2])  # avoid up and down
 
         if edgelength < pixelthd:
             diff = (pixelthd - edgelength) / 2.0
@@ -57,7 +53,6 @@ def ExtendBox(Boxes, dim, pixelthd=36):
             diff = (edgelength - pixelthd) / 2.0
             single_box[dim] = int(single_box[dim] + diff)
             single_box[dim + 2] = int(single_box[dim + 2] - diff)
-
 
 def BoxOverlap(Region1, Region2):
     """
@@ -72,8 +67,8 @@ def BoxOverlap(Region1, Region2):
     width  = min(Region1[2], Region2[2]) - max(Region1[0], Region2[0])
     height = min(Region1[1], Region2[1]) - max(Region1[3], Region2[3])
     intersection_area = width * height
-    region1_area = (Region1[1] - Region1[3]) * (Region1[2] - Region1[0])
-    region2_area = (Region2[1] - Region2[3]) * (Region2[2] - Region2[0])
+    region1_area = abs((Region1[1] - Region1[3])) * (Region1[2] - Region1[0])
+    region2_area = abs((Region2[1] - Region2[3])) * (Region2[2] - Region2[0])
     # another way:
     # if (Region1[0] > Region2[2])|(Region1[1] > Region2[3])|
     #     (Region1[2] < Region2[0])|(Region1[3] < Region2[1]):
@@ -81,14 +76,11 @@ def BoxOverlap(Region1, Region2):
     # Width = min(Region1[2], Region2[2]) - max(Region1[0], Region2[0])
     # Height = min(Region1[3], Region2[3]) - max(Region1[1], Region2[1])
     # IntersectionArea = Width * Height
-    # Area1 = (Region1[3] - Region1[1]) * (Region1[2] - Region1[0])
-    # Area2 = (Region2[3] - Region2[1]) * (Region2[2] - Region2[0])
 
     if region1_area + region2_area - intersection_area == 0:
         return 1000
     else:
         return intersection_area * 1.0 / (region1_area + region2_area - intersection_area)
-
 
 def MergeBoxes(boxes, threshold=0):
     """
@@ -128,8 +120,78 @@ def MergeBoxes(boxes, threshold=0):
 # ===============This is for Draw operation============
 # ===============This is for Draw operation============
 def DrawBoxes(boxes):
-    pass
+    for i in range(boxes.shape[0]):
+        color_set = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']  # notic: white
+        points = boxes[i, :]
+        plt.vlines(points[0], points[1], points[3], colors=color_set[i], label='ha')
+        plt.vlines(points[2], points[1], points[3], colors=color_set[i], label='ha')
+        plt.hlines(points[1], points[0], points[2], colors=color_set[i], label='ha')
+        plt.hlines(points[3], points[0], points[2], colors=color_set[i], label='ha')
+    return 1
 
+def ImageDrawLines(image, corner, color=(255, 0, 0)):
+    for i in range(4):
+        pass
+
+def ImageDrawLocation(image, all_location, color=(255, 0, 0)):
+    if all_location.size > 0:
+        if all_location.ndim == 1:
+            cv2.rectangle(image, (int(all_location[0]), int(all_location[1])),
+                          (int(all_location[2]), int(all_location[3])), color, 2)
+        else:
+            for SingleLocation in all_location:
+                cv2.rectangle(image, (int(SingleLocation[0]), int(SingleLocation[1])),
+                              (int(SingleLocation[2]), int(SingleLocation[3])), color, 2)
+
+
+# ====================================================
+# ===============This is for Image image image========
+# ===============This is for Image image image========
+def GenerateImageList(ImageListFile):
+    imagefile = open(ImageListFile, 'r')
+    ImageList = imagefile.readlines()
+    return ImageList
+
+
+# ====================================================
+# ===============Some Little Toys=====================
+# ===============Some Little Toys=====================
+def ShuffleFeature(trainfeature, trainlabel):
+    """&随机化训练数据"""
+    randomfeature = np.zeros(trainfeature.shape)
+    randomlabel = np.zeros(trainlabel.shape)
+    randomorder = np.arange(0, trainfeature.shape[0])
+    shuffle(randomorder)
+    for i in range(trainfeature.shape[0]):
+        randomfeature[i, :] = np.copy(trainfeature[randomorder[i], :])
+        randomlabel[i, :] = np.copy(trainlabel[randomorder[i], :])
+    return randomfeature, randomlabel
+
+
+def showImage(image):
+    plt.figure(1)
+    plt.imshow(image)
+    plt.show()
+
+
+def GenerateLabelForFeature(LabelNum, LabelDim, TrueDim):
+    _label = np.zeros((LabelNum, LabelDim))
+    _label[:, TrueDim] = 1
+    return _label
+
+
+def ReadFeatureFromFile(FeatureFile, Label, LabelDim):  # label 0 means pedestuirain, 1 means background
+    """&feature与label抽取"""
+    Feature = []
+    Label = []
+    for FeatureName in FeatureFile:
+        feature = np.load(FeatureName.split()[0])
+        label = GenerateLabelForFeature(feature.shape[0], LabelDim, Label)
+        Feature.extend(feature)
+        Label.extend(label)
+    Feature = np.asarray(Feature)
+    Label = np.asarray(Label)
+    return Feature, Label
 
 
 
