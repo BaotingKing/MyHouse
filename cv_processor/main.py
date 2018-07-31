0070c7727ff36b4457185e8c0093341f577c559c
BaotingKing <baoting.zhang@westwell-lab.com>
2018-07-31 15:05:27 +0800

    add draw UI function

--- cv_processor/main.py
+++ cv_processor/main.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import fitools
 import cv2
-
+import UI
 # boxes = np.arange(28).reshape(-1,4)
 # print(boxes)
 
@@ -76,3 +76,40 @@ arr = [[3, 56, 20, 12],
 # print("hi, after Shuffle randomlabel = ")
 # print(randomlabel)
 
+# ===========测试drawLetters函数=========
+# print("hi, 111111111111111111111111")
+# image = cv2.imread("cat.jpg")
+# resizedimage = cv2.resize(image, (1920, 1024))
+# UI.drawLetters(resizedimage, "abcd 1234567 6666 True", 2, 150)
+# cv2.imshow("cat", resizedimage)
+# print("hi, 222222222222222222222222")
+
+# ===========测试drawUI函数=========
+# print("hi, 111111111111111111111111")
+# image = cv2.imread("cat.jpg")
+# paper = np.zeros([1017, 1909, 3], np.uint8)
+# result = ['AAAA 0000000 0000 True', 'AAAA 0000000 0000 True']
+# # for i in range(5):
+# #         image = image
+# #         if i == 0:
+# #             resizedimage = cv2.resize(image, (560, 316))
+# #             paper[376:692, 1349:1909, 0] = resizedimage[:, :, 0]
+# #         elif i == 1:
+# #             resizedimage = cv2.resize(image, (560, 316))
+# #             paper[701:1017, 1349:1909, 1] = resizedimage[:, :, 1]
+# #         elif i == 2:
+# #             resizedimage = cv2.resize(image, (1329, 641))
+# #             paper[376:1017, 10:1339, 2] = resizedimage[:, :, 2]
+# #         elif i == 4:
+# #             resizedimage = cv2.resize(image, (560, 316))
+# #             paper[50:366, 1349:1909, :] = resizedimage
+# papers = np.zeros([2, paper.shape[0], paper.shape[1], 3])
+# images = np.zeros([4, image.shape[0], image.shape[1], 3])
+# papers[0, :,:,:] = paper.copy()
+# images[0, :,:,:] = image.copy()
+# images[1, :,:,:] = image.copy()
+# images[2, :,:,:] = image.copy()
+# images[3, :,:,:] = image.copy()
+#
+# paper = UI.drawUI(images, papers, result, 1,1,'1','1')
+# cv2.imshow('paper', paper)
