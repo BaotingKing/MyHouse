aac34c804c6d3de873b37fd3cbe73238df7dec2e
baotingzhang <baoting.zhang@westwell-lab.com>
2018-11-14 17:09:46 +0800

    update

---
+++ nnProject/stockAnaylsis/config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: wwl-ZK
+# Time: 2018/11/13
+
+g_fitting_size = 3
+
+g_max_close = 0
+g_min_close = 0
+
+
+def set_value(max_close=None, min_close=None):
+    global g_max_close
+    global g_min_close
+    g_max_close = max_close
+    g_min_close = min_close
+
+
+def get_value(name, d_value=None):
+    global g_max_close
+    global g_min_close
+    try:
+        if name == 'g_max_close':
+            return g_max_close
+        elif name == 'g_min_close':
+            return g_min_close
+    except KeyError:
+        return d_value
