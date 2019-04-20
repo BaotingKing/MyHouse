562654c3a6f37d836a17376bb1ea80d7ecf7473b
baotingzhang <baoting.zhang@westwell-lab.com>
2019-04-20 14:37:46 +0800

    clean pro

--- miDjango/mysite/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from blog.models import BlogsPost
-# Create your views here.
-
-
-def blog_index(request):
-    blog_list = BlogsPost.objects.all()  # 获取所有数据
-    return render(request,'index.html', {'blog_list':blog_list})   # 返回index.html页面
