658710adf12041fe46cdd43abdafb5278250d10b
BaotingKing <zhangbaoting_china@163.com>
2019-06-23 22:47:22 +0800

    little

--- nnProject/garden/field.py
+++ nnProject/garden/field.py
@@ -5,8 +5,7 @@
 import random
 import numpy as np
 import pandas as pd
-import tushare as ts
-import stock_network as snn
+import magic_network as snn
 from sklearn.model_selection import train_test_split
 
 # 获取数据
@@ -14,11 +13,13 @@ stock_code = ['000333', '600519', '601398', '999999']
 s_start = '2013-09-18'
 s_end = '2018-09-18'
 
+
 def vectorized_result(j, classes):
     """离散数据进行one-hot"""
     e = np.zeros((classes, 1))
     e[j] = 1.0
     return e
+
 
 def get_format_data(X, y, is_test):
     n_dim = X.shape[1]
@@ -34,35 +35,24 @@ def get_format_data(X, y, is_test):
     # data = zip(inputs, results)
     return data
 
+
 def normalize_data(data_source):
     min_data = data_source.min()
     max_data = data_source.max()
-    norm_data_source = (data_source[:] - min_data)/(max_data - min_data)
+    norm_data_source = (data_source[:] - min_data) / (max_data - min_data)
     return norm_data_source
 
 
 if __name__ == '__main__':
-    large_cap_data = ts.get_index()
-    stock_original_data = ts.get_hist_data('000333', start='2018-01-18', end='2018-09-08')
-    print("[Debug]This is original data: \n", stock_original_data.head(), type(stock_original_data))
+    n_dim = (5 + 2) * 2
+    out_dim = 5 + 2
+    original_seed_data = pd.read_csv('new_seed.csv')
 
-    stock_data = stock_original_data.copy()
-    stock_data.index = pd.DatetimeIndex(stock_original_data.index)
-    stock_data.ix[:, 'weekday'] = stock_data.index.weekday
-    print("[Debug]", stock_data.head(4), stock_data.tail(4))
-    print("[Debug]", stock_data.columns)
-    print('====================')
-    stock_normal_data = stock_data.sort_index(axis=0, ascending=True).copy()
-    for column in stock_data.columns:   # 数据归一化处理
-        if column != "weekday":
-            stock_normal_data[column] = normalize_data(stock_data[column])
+    seed_normal_data = original_seed_data.copy()
 
-    stock_normal_X = stock_normal_data.drop('close', axis=1)
-    stock_normal_y = stock_normal_data['close']
-    # print('this is 1', stock_normal_X.columns, stock_normal_X.head(6), type(stock_normal_X))
-    # print('this is 2', stock_normal_y.head(6), type(stock_normal_y))
-    # print('hahah:\n', stock_normal_data.head(6))
-    n_dim = stock_normal_X.shape[1]
+    stock_normal_X = seed_normal_data.drop('close', axis=1)
+    stock_normal_y = seed_normal_data['close']
+
     train_x, test_x, train_y, test_y = train_test_split(stock_normal_X,
                                                         stock_normal_y,
                                                         test_size=0.2,
@@ -71,22 +61,12 @@ if __name__ == '__main__':
     test_data = get_format_data(test_x, test_y, True)
 
     print("[Debug0]", type(training_data), len(training_data), type(test_data), len(test_data))
-    BP_size = [n_dim, 8, 6, 2, 1]
+    BP_size = [n_dim, n_dim * 256, n_dim * 16, n_dim * 3, out_dim]
     net = snn.StockNetwork(BP_size)
     net.SGD(training_data=training_data,
             epochs=5,
             mini_batch_size=10,
             learn_rate=0.1,
-            test_data=None)    # test_data None
+            test_data=None)  # test_data None
 
     print(type(net))
-
-
-
-
-
-
-
-
-
-
