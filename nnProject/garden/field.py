2f0298b066b39c8559e95ff95cf9b17dda090f2c
BaotingKing <zhangbaoting_china@163.com>
2019-06-24 23:22:27 +0800

    a little water

--- nnProject/garden/field.py
+++ nnProject/garden/field.py
@@ -8,11 +8,6 @@ import pandas as pd
 import magic_network as snn
 from sklearn.model_selection import train_test_split
 
-# 获取数据
-stock_code = ['000333', '600519', '601398', '999999']
-s_start = '2013-09-18'
-s_end = '2018-09-18'
-
 
 def vectorized_result(j, classes):
     """离散数据进行one-hot"""
@@ -50,23 +45,25 @@ if __name__ == '__main__':
 
     seed_normal_data = original_seed_data.copy()
 
-    stock_normal_X = seed_normal_data.drop('close', axis=1)
-    stock_normal_y = seed_normal_data['close']
+    seed_normal_X = seed_normal_data[:-1]     # 最后一个是要预测的待定值
+    seed_normal_y = seed_normal_data[1:]
 
-    train_x, test_x, train_y, test_y = train_test_split(stock_normal_X,
-                                                        stock_normal_y,
+    print(len(seed_normal_data), len(seed_normal_X), len(seed_normal_y))
+    train_x, test_x, train_y, test_y = train_test_split(seed_normal_X,
+                                                        seed_normal_y,
                                                         test_size=0.2,
                                                         random_state=0)
     training_data = get_format_data(train_x, train_y, False)
     test_data = get_format_data(test_x, test_y, True)
 
-    print("[Debug0]", type(training_data), len(training_data), type(test_data), len(test_data))
-    BP_size = [n_dim, n_dim * 256, n_dim * 16, n_dim * 3, out_dim]
-    net = snn.StockNetwork(BP_size)
-    net.SGD(training_data=training_data,
-            epochs=5,
-            mini_batch_size=10,
-            learn_rate=0.1,
-            test_data=None)  # test_data None
-
-    print(type(net))
+    print("[Debug]", len(seed_normal_data), len(seed_normal_X), len(seed_normal_y))
+    print("[Debug]", type(training_data), len(training_data), type(test_data), len(test_data))
+    # BP_size = [n_dim, n_dim * 256, n_dim * 16, n_dim * 3, out_dim]
+    # net = snn.StockNetwork(BP_size)
+    # net.SGD(training_data=training_data,
+    #         epochs=5,
+    #         mini_batch_size=10,
+    #         learn_rate=0.1,
+    #         test_data=None)  # test_data None
+    #
+    # print(type(net))
